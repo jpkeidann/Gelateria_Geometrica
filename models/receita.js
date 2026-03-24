6cf4ad2191de945bcd994ab7479965663815fa6d
davi Pereira Fagundes <davi_p_fagundes@estudante.sesisenai.org.br>
2026-03-24 08:59:00 -0300

    arrumado

--- models/receita.js
+++ models/receita.js
@@ -1,19 +1,20 @@
 export default class Receita {
-    constructor() {
-        // base de 900g (doce de leite)
-        this.leite = 500
-        this.acucar = 200
-        this.creme = 150
-        this.doceLeite = 50
+    // valores padrão da receita base de 900g (doce de leite)
+    constructor(leite = 500, acucar = 200, creme = 150, doceLeite = 50) {
+        this.leite = leite
+        this.acucar = acucar
+        this.creme = creme
+        this.doceLeite = doceLeite
 
+        // peso base da receita de 900 gramas
         this.pesoBase = this.leite + this.acucar + this.creme + this.doceLeite
         this.receita = {}
+        this.totalPotes = 0
     }
 
-    calcularIngredientes(toneladas) {
-        const totalGramas = toneladas * 1000000
-        const fator = totalGramas / this.pesoBase
-        
+    calcularQtdeIngredientes(toneladas = 1) {
+        const fator = (toneladas * 1000000) / this.pesoBase
+
         this.receita = {
             leite: Number((this.leite * fator).toFixed(2)),
             acucar: Number((this.acucar * fator).toFixed(2)),
@@ -22,5 +23,16 @@ export default class Receita {
         }
 
         return this.receita
+    }
+
+    calcularQtdePotes(pesoUnitario, toneladas = 1) {
+        this.totalPotes = (toneladas * 1000000) / pesoUnitario
+        this.verificarQtdePotes()
+        return this.totalPotes
+    }
+
+    verificarQtdePotes() {
+        // regra de negócio: apenas potes inteiros
+        this.totalPotes = Math.floor(this.totalPotes)
     }
 }
