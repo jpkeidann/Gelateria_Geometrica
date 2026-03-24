4f91d175a8380354fbba6718fe3055cfcda4d4fa
abel silva neto <abel_silva-neto@estudante.sesisenai.org.br>
2026-03-24 09:18:45 -0300

    Custo

---
+++ models/custo.js
@@ -0,0 +1,36 @@
+export default class Custo {
+    constructor(leite = 3.50, acucar = 4.89, doceDeLeite = 18.00, creme = 12.00) {
+        // preço por kg
+        this.leite = leite
+        this.acucar = acucar
+        this.doceDeLeite = doceDeLeite
+        this.creme = creme
+
+
+        this.totalCusto = 0
+        this.preco = {}
+    }
+
+
+    calcularCusto(ingredientes) {
+        const total =
+            (ingredientes.leite / 1000) * this.leite +
+            (ingredientes.acucar / 1000) * this.acucar +
+            (ingredientes.doceDeLeite / 1000) * this.doceDeLeite +
+            (ingredientes.creme / 1000) * this.creme
+
+
+        this.totalCusto = Number(total.toFixed(2))
+
+
+        return this.totalCusto
+    }
+
+
+    custoPorPote(totalCusto, qtdPotes ) {
+        return Number((totalCusto / qtdPotes).toFixed(2))
+    }
+
+
+   
+}
