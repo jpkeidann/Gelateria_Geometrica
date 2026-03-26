44f54cc8100b52dfeae6f22d90bb832989603df5
abel silva neto <abel_silva-neto@estudante.sesisenai.org.br>
2026-03-26 09:00:42 -0300

    troquei DoceDeLeite para DoceLeite

--- models/custo.js
+++ models/custo.js
@@ -1,9 +1,9 @@
 export default class Custo {
-    constructor(leite = 3.50, acucar = 4.89, doceDeLeite = 18.00, creme = 12.00) {
+    constructor(leite = 3.50, acucar = 4.89, doceLeite = 18.00, creme = 12.00) {
         // preço por kg
         this.leite = leite
         this.acucar = acucar
-        this.doceDeLeite = doceDeLeite
+        this.doceLeite = doceLeite
         this.creme = creme
 
 
@@ -16,7 +16,7 @@ export default class Custo {
         const total =
             (ingredientes.leite / 1000) * this.leite +
             (ingredientes.acucar / 1000) * this.acucar +
-            (ingredientes.doceDeLeite / 1000) * this.doceDeLeite +
+            (ingredientes.doceLeite / 1000) * this.doceLeite +
             (ingredientes.creme / 1000) * this.creme
 
 
